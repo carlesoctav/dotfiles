67a7d4607b34f02f7f712037bb597705dbb7161e
carlesoctav <carlesoctavianus@tuta.io>
2024-07-02 12:43:31 +0700

    quality of life, supermaven > copilot?

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,5 @@
 require "options"
 
-
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -12,6 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   }
 end
+
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   spec = {
--- tasks/run.sh
+++ tasks/run.sh
@@ -1,2 +1,1 @@
-
-./dotfiles.sh && ./tmux.sh && ./python.sh && ./node.sh && ./go.sh && ./rust.sh && ./tooling.sh && ./nvim.sh
+./tmux.sh && ./python.sh && ./node.sh && ./go.sh && ./rust.sh && ./tooling.sh && ./nvim.sh
