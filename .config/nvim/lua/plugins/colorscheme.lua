fd2f933b2e9c7af35a9bc8215ccef35fbc8f2306
carlesoctav <carlesoctavianus@tuta.io>
2024-07-22 12:33:30 +0700

    add transparancy so i can see kobo :D

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,13 @@
 return {
     {'rebelot/kanagawa.nvim',
     config = function()
+        require("kanagawa").setup({
+                transparant = true,
+                theme = "kanagawa-wave"
+            })
         vim.cmd('colorscheme kanagawa-wave')
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
 }
     
