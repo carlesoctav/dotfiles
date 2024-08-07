c4e9ba4fc1eba24db74b10de53788941b168b4a7
carlesoctav <carlesoctavianus@tuta.io>
2024-08-07 16:28:41 +0700

    idk

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -46,15 +46,7 @@ return {
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 		local servers = {
-			pyright = {
-				settings = {
-					pyright = {
-						-- Using Ruff's import organizer
-						disableOrganizeImports = true,
-					},
-				},
-				disableOrganizeImports = true,
-			},
+			pyright = {},
 			lua_ls = {
 				settings = {
 					Lua = {
