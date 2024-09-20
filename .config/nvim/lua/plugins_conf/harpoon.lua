62908aafa6df2bc7297949b52d80b789d413e50e
carlesoctav <carlesoctavianus@tuta.io>
2024-09-20 10:23:16 +0700

    keybding

--- .config/nvim/lua/plugins_conf/harpoon.lua
+++ .config/nvim/lua/plugins_conf/harpoon.lua
@@ -1,10 +1,10 @@
 local harpoon = require("harpoon")
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>ah", function()
+vim.keymap.set("n", "<leader>a", function()
 	harpoon:list():add()
 end)
-vim.keymap.set("n", "<leader>al", function()
+vim.keymap.set("n", "<C-e>", function()
 	harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
 
