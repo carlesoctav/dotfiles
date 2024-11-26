83ca5925e6c35b82af2a8f6e6e50f16fa054466a
carlesoctav <carlesoctavianus@tuta.io>
2024-11-26 20:15:28 +0700

    better script

--- tpu-tasks/nfs_server.sh
+++ tpu-tasks/nfs_server.sh
@@ -2,6 +2,7 @@ sudo apt-get install -y -qq nfs-kernel-server
 sudo mkdir -p /nfs_share
 sudo chown -R nobody:nogroup /nfs_share
 sudo chmod 777 /nfs_share
+internal_ip=$(hostname -I | awk '{print $1}')
+echo "/nfs_share  ${internal_ip}/24(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
 sudo exportfs -ra
 sudo systemctl restart nfs-kernel-server
-
