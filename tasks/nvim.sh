0498b68725edeb79bb50743fa1a6420850b82704
carlesoctav <carlesoctavianus@tuta.io>
2025-02-17 11:37:12 +0700

    fix: nvim installtion (new link)

--- tasks/nvim.sh
+++ tasks/nvim.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Download the latest Neovim AppImage
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+curl -o ./nvim.appimage -LO https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.appimage
 
 # Make the AppImage executable
-chmod +x nvim.appimage
+chmod +x nvim.appimage 
+
 sudo mv ./nvim.appimage /usr/local/bin/nvim
 nvim
+
