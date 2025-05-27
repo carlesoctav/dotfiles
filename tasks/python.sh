aeece1ad3f4eb1cc4ae23e07d0b2e4e3f68fe699
carlesoctav <carlesoctavianus@tuta.io>
2025-05-27 10:13:26 +0700

    add uv

--- tasks/python.sh
+++ tasks/python.sh
@@ -3,3 +3,4 @@ sudo apt install -y build-essential zlib1g-dev libssl-dev
 sudo apt install -y libreadline-dev libsqlite3-dev libffi-dev libbz2-dev liblzma-dev
 curl -sS https://webi.sh/pyenv | sh
 curl -sSL https://install.python-poetry.org | python3 -
+curl -LsSf https://astral.sh/uv/install.sh | sh
