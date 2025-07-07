70502c87067cff5ae55df6842154f5565fb98e2b
carlesoctav <carlesoctavianus@tuta.io>
2025-07-07 17:36:12 +0700

    no need gitsigns

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,15 +1,5 @@
 return {
 	{
-		"echasnovski/mini.diff",
-		config = function()
-			local diff = require("mini.diff")
-			diff.setup({
-				source = diff.gen_source.none(),
-			})
-		end,
-	},
-
-	{
 		"olimorris/codecompanion.nvim",
 
 		dependencies = {
