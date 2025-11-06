b63214a40e355908e947826ea8117f2bccee5653
carlesoctav <carlesoctavianus@tuta.io>
2025-11-06 22:55:11 +0700

    update ubuntu

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,13 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
+        lazy = false,
+        branch = 'master',
+        build = ':TSUpdate',
         config = function()
             require("plugins_conf.treesitter")
         end,
 
     },
-    {'nvim-treesitter/nvim-treesitter-textobjects'},
+    { 'nvim-treesitter/nvim-treesitter-textobjects' },
 }
--- tasks/tooling.sh
+++ tasks/tooling.sh
@@ -2,14 +2,7 @@ curl -sS https://webi.sh/fd | sh
 curl -sS https://webi.sh/rg | sh
 curl -sS https://webi.sh/gh | sh
 curl -sS https://webi.sh/jq | sh
-curl -sS https://webi.sh/bat | sh
-curl -sS https://webi.sh/curlie | sh
-curl -sS https://webi.sh/dotenv | sh
-curl -sS https://webi.sh/aliasman | sh
-curl -sS https://webi.sh/xsv | sh
-curl -sS https://webi.sh/yq | sh
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 sudo apt install unzip
-
